5dc0ae243eb844b100bffd562337e6164428b89a
Alex Kolarski <aleks.rk@gmail.com>
2019-05-22 17:05:30 +0300

    Ignore .vscode, Added Travis CI integration

--- src/recognizers/DateTime.ts
+++ src/recognizers/DateTime.ts
@@ -5,20 +5,16 @@ import moment from "moment";
 export default class DateTime implements IRecognizer {
   name: string = "datetime";
   recognize(text: string): IRecognizedType | null {
-    try {
-      // Do not recognize integers as dates
-      const isInt = /^-{0,1}\d+$/.test(text);
+    // Do not recognize integers as dates
+    const isInt = /^-{0,1}\d+$/.test(text);
 
-      if (moment(new Date(text)).isValid() && !isInt) {
-        return {
-          type: this.name,
-          value: text,
-          sanatizedValue: moment(new Date(text)).toString()
-        };
-      }
-      return null;
-    } catch (err) {
-      return null;
+    if (moment(new Date(text)).isValid() && !isInt) {
+      return {
+        type: this.name,
+        value: text,
+        sanatizedValue: moment(new Date(text)).toString()
+      };
     }
+    return null;
   }
 }
