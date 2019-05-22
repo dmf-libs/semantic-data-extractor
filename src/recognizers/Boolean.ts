883e57a7c788856e6411576f464cd032d497d205
Alex Kolarski <aleks.rk@gmail.com>
2019-05-22 17:22:13 +0300

    Refactored Boolean Recognized (extracted booleanValues)

--- src/recognizers/Boolean.ts
+++ src/recognizers/Boolean.ts
@@ -3,27 +3,30 @@ import IRecognizedType from "../interfaces/IRecognizedType";
 
 export default class Boolean implements IRecognizer {
   name: string = "boolean";
+  recognizeBoolean(
+    text: string,
+    boolValues: Array<string>
+  ): IRecognizedType | null {
+    if (boolValues.indexOf(text.toLowerCase()) !== -1) {
+      return {
+        type: this.name,
+        value: text,
+        sanatizedValue: text.toLowerCase() === boolValues[0]
+      };
+    }
+    return null;
+  }
+  boolValuesArray: Array<Array<string>> = [
+    ["yes", "no"],
+    ["true", "false"],
+    ["1", "0"]
+  ];
   recognize(text: string): IRecognizedType | null {
-    if (["yes", "no"].indexOf(text.toLowerCase()) !== -1) {
-      return {
-        type: this.name,
-        value: text,
-        sanatizedValue: text.toLowerCase() === "yes"
-      };
-    }
-    if (["true", "false"].indexOf(text.toLowerCase()) !== -1) {
-      return {
-        type: this.name,
-        value: text,
-        sanatizedValue: text.toLowerCase() === "true"
-      };
-    }
-    if (["1", "0"].indexOf(text) !== -1) {
-      return {
-        type: this.name,
-        value: text,
-        sanatizedValue: text === "1"
-      };
+    for (let boolValues of this.boolValuesArray) {
+      const result = this.recognizeBoolean(text, boolValues);
+      if (result !== null) {
+        return result;
+      }
     }
     return null;
   }
